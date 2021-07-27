5230cfd4af003528104c0b1c25c4a8122b57fdf9
pratikdk <pratik.deoolwadikar@gmail.com>
2021-07-27 16:23:50 +0530

    initial dump

---
+++ src/test/java/course/sf/sfg_externalized_properties/SfgExternalizedPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package course.sf.sfg_externalized_properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgExternalizedPropertiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
